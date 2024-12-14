db616378bf3eb44b0705ad995dc9e332fdac52cd
HusanjonDeveloper <muhammadaliyevhusanjon181@gmail.com>
2024-12-14 20:40:23 +0500

    new

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("contact-form").addEventListener("submit", function(event) {
+    event.preventDefault();
+    alert("Xabaringiz yuborildi. Tez orada siz bilan bog'lanamiz!");
+});
